8c6e7f1a2e710ca1200822f13b1b9bf37df25801
Fawwazul <mhmdfwwz7@gmail.com>
2026-04-15 11:40:50 +0700

    fix error tsconfig

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,13 @@
 // prisma.config.ts
 import { defineConfig } from '@prisma/config';
+import { config } from 'dotenv';
+
+// Baris ini SANGAT KRUSIAL agar sistem bisa membaca file .env
+config();
 
 export default defineConfig({
-  // Kita ganti 'migrate' menjadi 'datasource' sesuai permintaan error Prisma
   datasource: {
+    // Sekarang sistem pasti bisa menemukan URL dari Supabase
     url: process.env.DATABASE_URL as string,
   },
 });
